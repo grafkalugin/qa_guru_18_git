84c23aa9071837d06ab7fb5f2ea8d3f46a0df525
Александр Калугин <graf.kalugin@gmail.com>
2023-02-25 13:51:00 +0100

    Update MainPageTest.java

--- src/test/java/com/example/qa_guru_18_git/MainPageTest.java
+++ src/test/java/com/example/qa_guru_18_git/MainPageTest.java
@@ -17,7 +17,7 @@ public class MainPageTest {
 
     @BeforeAll
     public static void setUpAll() {
-        Configuration.browserSize = "1280x802";
+        Configuration.browserSize = "1280x800";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
