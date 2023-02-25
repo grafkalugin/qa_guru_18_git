8cb27f772000a785cd5bc3647b8172d60d4fada0
Калугин Александр Александрович <AKalugin2@alfabank.ru>
2023-02-25 13:56:51 +0100

    size5forConflict

--- src/test/java/com/example/qa_guru_18_git/MainPageTest.java
+++ src/test/java/com/example/qa_guru_18_git/MainPageTest.java
@@ -17,7 +17,7 @@ public class MainPageTest {
 
     @BeforeAll
     public static void setUpAll() {
-        Configuration.browserSize = "1280x804";
+        Configuration.browserSize = "1280x805";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
